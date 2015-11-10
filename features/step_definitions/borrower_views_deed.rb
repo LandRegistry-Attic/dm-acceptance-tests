1e2d79bb9d0879b14cfccf0ad13c7ae1b9f185ed
cs112mm <mandy.nimmo@digital.landregistry.gov.uk>
2015-11-10 14:37:34 +0000

    acceptance tests for borrower views deed

---
+++ features/step_definitions/borrower_views_deed.rb
@@ -0,0 +1,34 @@
+
+Given(/^I have created a deed with two borrowers$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I navigate to the borrower frontend "([^"]*)" page$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I search for the created deed$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the “Your mortgage deed" page is displayed$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+And(/^the borrowers are displayed on the deed$/) do |table|
+  # table is a Cucumber::Core::Ast::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+And(/^the property is displayed on the deed$/) do |table|
+  # table is a Cucumber::Core::Ast::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I search for an invalid deed$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the "([^"]*)" page is displayed$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
