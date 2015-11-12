68affecca24580eee1c83d0d54c396bfd54433b4
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2015-11-12 10:13:15 +0000

    added code to generate valid/invalid title data

--- features/step_definitions/us6_add_title_number_to_deed.rb
+++ features/step_definitions/us6_add_title_number_to_deed.rb
@@ -1,7 +1,15 @@
-Given(/^I have a valid title number$/) do
-  px = Pxeger.new('^([A-Z]{0,3}[1-9][0-9]{0,5}|[0-9]{1,6}[ZT])$')
-  @title_number = px.generate
-  #puts @title_number.to_s
+Given(/^I have a valid title$/) do
+  @title = create_title_hash.to_json
+end
+
+Given(/^I have a title with two title numbers$/) do
+  @title = {
+    'title-number' => [generate_title_no, generate_title_no]
+  }.to_json
+end
+
+Given(/^I have a title with an invalid title number$/) do
+  @title = create_title_hash('ABCD123/EF').to_json
 end
 
 When(/^I send a valid title number$/) do
