291ecc62db37a43429865c8c5bdf8acc9bb8bfd1
marknorman <mark@kirosoft.co.uk>
2015-11-19 16:13:48 +0000

    Update test to call common function with generated borrower data

--- features/step_definitions/us5_provide_borrower_names.rb
+++ features/step_definitions/us5_provide_borrower_names.rb
@@ -1,9 +1,9 @@
 Given(/^I have a deed with a borrower with no middle name$/) do
-  @deed = create_deed_hash_no_mid
+  @deed = create_deed_hash(generate_title_number, generate_new_borrower_no_middle)
 end
 
 Given(/^I have a deed with a borrower with a middle name$/) do
-  @deed = create_deed_hash_with_mid
+  @deed = create_deed_hash(generate_title_number, generate_new_borrower_with_middle)
 end
 
 Given(/^I have a deed with no borrowers$/) do
