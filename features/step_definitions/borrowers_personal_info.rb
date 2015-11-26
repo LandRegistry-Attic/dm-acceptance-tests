f9bf2c2345cf7dffe9be610c85ebc483377c33ac
cs112mm <mandy.nimmo@digital.landregistry.gov.uk>
2015-11-26 13:55:28 +0000

    removed puts statement from create data file

--- features/step_definitions/borrowers_personal_info.rb
+++ features/step_definitions/borrowers_personal_info.rb
@@ -1,7 +1,6 @@
 Given(/^I add the following deed:$/) do |deed|
   deed_end_point = Env.deed_api + '/deed/'
   @response = HTTP.post(deed_end_point, json: JSON.parse(deed))
-  puts @response
 end
 
 And(/^a message for failure is given "([^"]*)"$/) do |failure_reason|
