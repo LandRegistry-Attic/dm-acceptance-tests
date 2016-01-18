b76180cd1c89a39dd110e69870c39ac99432157d
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2016-01-18 15:18:57 +0000

    wrote tests for error handling for borrower enters date

--- features/step_definitions/us103_borrower_enters_dob.rb
+++ features/step_definitions/us103_borrower_enters_dob.rb
@@ -7,17 +7,27 @@ When(/^I enter the borrowers date of birth$/) do
 end
 
 When(/^I enter an incorrect date of birth$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  split_dob = @deed.borrowers[0][:dob].split('/')
+  fill_in 'dob-day', with: split_dob[0]
+  fill_in 'dob-month', with: split_dob[1]
+  fill_in 'dob-year', with: (split_dob[2].to_i - 2).to_s
+  click_button('Continue')
 end
 
 When(/^I enter a date of birth that is in the future$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in 'dob-day', with: "20"
+  fill_in 'dob-month', with: "10"
+  fill_in 'dob-year', with: "2099"
+  click_button('Continue')
 end
 
 When(/^I enter a date of birth that is not in a uk date format$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in 'dob-day', with: "09"
+  fill_in 'dob-month', with: "30"
+  fill_in 'dob-year', with: "2015"
+  click_button('Continue')
 end
 
 Then(/^the error "([^"]*)" should be displayed$/) do |error_message|
-  pending # Write code here that turns the phrase above into concrete actions
+  page.should have_content(error_message)
 end
