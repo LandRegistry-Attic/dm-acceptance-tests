48a42bbfbdc0bfe086e25988a63052882260b87e
Aaron Flynn <aaronflynn@LRAdmins-MacBook-Pro.local>
2016-02-04 13:58:04 +0000

    Improved deed creation and search methods. Test Data is created as feature file step.

--- features/step_definitions/us152_search_for_deed_via_title_number_and_md_ref.rb
+++ features/step_definitions/us152_search_for_deed_via_title_number_and_md_ref.rb
@@ -1,5 +1,5 @@
 When(/^I search the deed store$/) do
-  str_end = '/deed?md_ref=' + @md_ref.to_s + '&title_number='
+  str_end = '/deed?md_ref=e-MD12344&title_number='
   deed_end_point = Env.deed_api + str_end + @title_number.to_s
   @response = HTTP.get(deed_end_point)
 
@@ -9,26 +9,23 @@ When(/^I search the deed store$/) do
   visit(full_str)
 end
 
-When(/^I search the deed store with title_number <(\d+)>$/) do |title_number|
-  head = 'http://10.10.10.10:9020/deed?md_ref=e-MD12344&title_number=DT'
+When(/^I search the deed store with title_number "([^"]*)"$/) do |title_number|
+  # Open the URL in browser to check information
+  head = 'http://10.10.10.10:9020/deed?md_ref=e-MD12344&title_number='
   full_str = head + title_number.to_s
   visit(full_str)
 end
 
-Given(/^I create the deed with title_number <(\d+)>$/) do |title_num|
+Given(/^I create the deed with title_number "([^"]*)"$/) do |title_num|
   # Using to create deed with specific title number
-=begin
   @deed = Deed.new(1)
   @deed.title_number = title_num
   @deed_hash = @deed.to_hash
-=end
-  require 'pry'
-  binding.pry
-  str_end = '/deed?md_ref=e-MD12344&title_number=DT' + title_num.to_s
-  deed_end_point = Env.deed_api + str_end
-  @response = HTTP.get(deed_end_point)
+end
 
-  head = 'http://10.10.10.10:9020/deed?md_ref=e-MD12344&title_number=DT'
-  full_str = head + title_num.to_s
-  visit(full_str)
+Given(/^I create the 2nd deed with title_number <(\d+)>$/) do |title_num|
+  # Using to create deed with specific title number
+  @deed2 = Deed.new(1)
+  @deed2.title_number = title_num
+  @deed_hash2 = @deed2.to_hash
 end
