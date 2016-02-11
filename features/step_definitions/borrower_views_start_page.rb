1b4701fdd28bb4eac33d0b4e22d385bca4949abd
cs112mm <mandy.nimmo@digital.landregistry.gov.uk>
2016-02-11 10:46:40 +0000

    us150 acceptance tests

--- features/step_definitions/borrower_views_start_page.rb
+++ features/step_definitions/borrower_views_start_page.rb
@@ -12,3 +12,17 @@ Then(/^information about the service and how to use it is displayed$/) do
   page.should have_content('a unique borrower reference sent to you by your '\
                            'conveyancer')
 end
+
+Then(/^the conform with Data Protection Law text is displayed$/) do
+  page.should have_content('By using this service you consent to Land '\
+                           'Registry retaining your personal details for '\
+                           'fraud prevention and detection purposes. You also '\
+                           'consent to Land Registry carrying out third party '\
+                           'checks in relation to the information supplied. '\
+                           'For further information please view our '\
+                           'Personal Information Charter')
+end
+
+  And(/^a link to Personal Information Charter is displayed$/) do
+    page.should have_link('Personal Information Charter.', href: 'https://www.gov.uk/government/organisations/land-registry/about/personal-information-charter')
+ end
