7da4e5e5c40b3ac2669ce79f7fcd99a484c71eac
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2016-02-17 11:58:51 +0000

    added step to call the get pdf endpoint with incorrect deed_id

--- features/step_definitions/lr_retrieves_pdf.rb
+++ features/step_definitions/lr_retrieves_pdf.rb
@@ -2,3 +2,8 @@ When(/^I retrieve a pdf using the deed id$/) do
   get_pdf_endpoint = Env.deed_api + '/casework/' + @deed_id
   @response = HTTP.get(get_pdf_endpoint)
 end
+
+When(/^I retrieve a pdf using an incorrect deed id$/) do
+  get_pdf_endpoint = Env.deed_api + '/casework/1234567'
+  @response = HTTP.get(get_pdf_endpoint)
+end
