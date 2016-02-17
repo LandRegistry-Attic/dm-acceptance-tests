ddc5501fbd6fb18305a251ede0905b837d4b202c
Aaron Flynn <aaronflynn@LRAdmins-MacBook-Pro.local>
2016-02-17 14:53:35 +0000

    Step takes in optional param.[WIP] needs more research. Values not appliying as expected

--- features/step_definitions/testdebt-generic-test-data-step.rb
+++ features/step_definitions/testdebt-generic-test-data-step.rb
@@ -1,5 +1,7 @@
-Then(/^I update deed variable "([^"]*)" with value "([^"]*)"$/) do |var, value|
+#When (/^I check the favorite color count(?: for email address (.*))?$/) do |email|
+Then(/^I update deed variable "([^"]*)" with value "([^"]*)"(?:, borrower "([^"]*)")?$/) do |var, value, bor|
   #Take in array of params. Perform on all lines
+  opt ||= 1 # This value is global in the test, i.e get updated between steps
   case var
   when 'title_number'
     @deed.title_number = value
@@ -8,26 +10,29 @@ Then(/^I update deed variable "([^"]*)" with value "([^"]*)"$/) do |var, value|
   when 'identity_checked'
     @deed.identity_checked = value
   when 'forename'
-    @deed.borrowers[0][:forename] = value
+    @deed.borrowers[bor.to_i - 1][:forename] = value
   when 'surname'
-    @deed.borrowers[0][:surname] = value
+    @deed.borrowers[bor.to_i - 1][:surname] = value
   when 'gender'
-    @deed.borrowers[0][:gender] = value
+    @deed.borrowers[bor.to_i - 1][:gender] = value
   when 'address'
-    @deed.borrowers[0][:address] = value
+    @deed.borrowers[bor.to_i - 1][:address] = value
   when 'dob'
-    @deed.borrowers[0][:dob] = value
+    @deed.borrowers[bor.to_i - 1][:dob] = value
   when 'phone_number'
-    @deed.borrowers[0][:phone_number] = value
+    @deed.borrowers[bor.to_i - 1][:phone_number] = value
   when 'property_address'
-    @deed.borrowers[0][:property_address] = value
+    @deed.borrowers[bor.to_i - 1][:property_address] = value
   when 'middle_name'
-    @deed.borrowers[0][:middle_name] = value
+    @deed.borrowers[bor.to_i - 1][:middle_name] = value
   else
     abort('Unrecognised Input, please recheck variable name, and value.')
   end
+  opt = 1 #resetting default value
 end
 
 Then(/^I hash the deed$/) do
+  require 'pry'
+  binding.pry
   @deed_hash = @deed.to_hash
 end
--- features/testdebt-generic-test-data-step.feature
+++ features/testdebt-generic-test-data-step.feature
@@ -3,20 +3,24 @@ Feature: Override deed variables
 
 Acceptance Criteria
 - Can override ALL deed variables from one step
+
 @techdebt
 Scenario: Override identity_checked variable
-Given I have valid deed data with <1> borrowers
+Given I have valid deed data with <2> borrowers
 And I update deed variable "title_number" with value "009"
 And I update deed variable "md_ref" with value "Tt109"
 And I update deed variable "identity_checked" with value "Y"
-And I update deed variable "forename" with value "Nick"
-And I update deed variable "surname" with value "Valentine"
+And I update deed variable "forename" with value "Nick", borrower "2"
+And I update deed variable "surname" with value "Valentine", borrower "2"
 And I update deed variable "gender" with value "Male"
 And I update deed variable "address" with value "34A Other Address, Actual Address"
 And I update deed variable "dob" with value "01/02/1986"
 And I update deed variable "phone_number" with value "12345678"
 And I update deed variable "property_address" with value "123 Fake St, Fake Town"
-And I update deed variable "middle_name" with value "Synth"
+And I update deed variable "middle_name" with value "Synth", borrower "2"
+Then I hash the deed
+And I update deed variable "forename" with value "Nany"
+And I update deed variable "surname" with value "Geoff"
 Then I hash the deed
 
 @techdebt
