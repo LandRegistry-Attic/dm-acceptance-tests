c62af54693dc38dd6bd1b8dbd98d097e42031ee3
Aaron Flynn <aaronflynn@LRAdmins-MacBook-Pro.local>
2016-02-29 11:17:17 +0000

    Sets deed params dynamically. Deed is not yet being updated correctly

--- features/step_definitions/testdebt_generic_test_data_step.rb
+++ features/step_definitions/testdebt_generic_test_data_step.rb
@@ -1,38 +1,20 @@
-Then(/^I override deed with "([^"]*)"$/) do |params|
-  # Example use ... And I override deed with "middle_name:Synth:2"
-
-  # Splits the params string
-  split_params = params.split(':')
-  var = split_params[0]
-  value = split_params[1]
-  opt = split_params[2]
-
-  # Applies deed override
-  case var
-  when 'title_number'
-    @deed.title_number = value
-  when 'md_ref'
-    @deed.md_ref = value
-  when 'identity_checked'
-    @deed.identity_checked = value
+Then(/^set "([^"]*)" to "([^"]*)" for borrower <(\d+)>$/) do |var, value, opt|
+  # Applies the deed override
+  if var == 'title_number' || var == 'md_ref' || var == 'identity_checked'
+    @deed.instance_variable_set("@#{var}", value)
   else
     @deed.borrowers[opt.to_i - 1][:"#{var}"] = value
   end
-  step %(I hash the deed)
 end
 
 Then(/^I hash the deed$/) do
   @deed_hash = @deed.to_hash
 end
 
-Given(/^I create default deed with <(\d+)> borrowers(?:,"([^"]*)")?$/) do |borrower, params|
+# Performs deed setup Version 1.0
+Given(/^I create default deed with <(\d+)> borrowers$/) do |borrower|
   # Creates a deed, with entered number of [borrowers].
-  # Uses [params] to apply any overrides to the deed. e.g Change the name
   step %(I have valid deed data with <#{borrower}> borrowers)
-  #Checks for optional deed params
-  unless params.nil?
-    step %(I override deed with "#{params}")
-  end
   step %(I create the deed via the Deed API)
   step %(the deed id is returned by the Deed API)
   step %(I retrieve the unique user id for borrower <#{borrower}>)
--- features/testdebt_generic_test_data_step.feature
+++ features/testdebt_generic_test_data_step.feature
@@ -10,16 +10,11 @@ Given I create default deed with <2> borrowers
 And borrower <2> views the deed
 And borrower <1> views the deed
 
+# version 1.0
+# Readable way to override deed info
 @techdebt @techdebt2
 Scenario: WIP for override
 Given I create default deed with <2> borrowers
-And I override deed with "forename:TIMMY:2"
-And I override deed with "middle_name:TIMMY:2"
-And I override deed with "surname:TIMMY:2"
+Then set "forename" to "Tinny" for borrower <1>
+Then set "dob" to "22/2/1982" for borrower <2>
 And I hash the deed
-
-@techdebt @techdebt3
-Scenario:  WIP create deed with override
-Given I create default deed with <3> borrowers,"forename:TIMMY:2"
-And borrower <2> views the deed
-And borrower <1> views the deed
