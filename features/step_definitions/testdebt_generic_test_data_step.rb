9f97295b328e1f9747fa93c804173753efad8d0b
Aaron Flynn <aaronflynn@LRAdmins-MacBook-Pro.local>
2016-03-01 10:35:42 +0000

    Removed the step which ammends a single value. Also applied wording changes to features.

--- features/step_definitions/testdebt_generic_test_data_step.rb
+++ features/step_definitions/testdebt_generic_test_data_step.rb
@@ -1,25 +1,11 @@
-Then(/^set "([^"]*)" to "([^"]*)" for borrower <(\d+)>$/) do |var, value, opt|
+And(/^I ammend "([^"]*)" to "([^"]*)" for borrower <(\d+)>$/) do |var, val, opt|
   # Applies the deed override
-  if var == 'title_number' || var == 'md_ref' || var == 'identity_checked'
-    @deed.instance_variable_set("@#{var}", value)
-  else
-    @deed.borrowers[opt.to_i - 1][:"#{var}"] = value
-  end
-  step %(I hash the deed)
-end
-
-# Set up 1 borrower deed with one change -- Covers alot of existing tests
-Given(/^deed data with "([^"]*)" as "([^"]*)"$/) do |val, var|
-  @deed = Deed.new(1)
-
   if var == 'title_number' || var == 'md_ref' || var == 'identity_checked'
     @deed.instance_variable_set("@#{var}", val)
   else
-    @deed.borrowers[0][:"#{var}"] = val
+    @deed.borrowers[opt.to_i - 1][:"#{var}"] = val
   end
   step %(I hash the deed)
-  step %(I create the deed via the Deed API)
-  step %(the deed id is returned by the Deed API)
 end
 
 Then(/^I hash the deed$/) do
@@ -35,7 +21,7 @@ Given(/^I create default deed with <(\d+)> borrowers$/) do |borrower|
 end
 
 # Use before any overrides
-Given(/^I setup deed with <(\d+)> borrowers$/) do |borrower|
+Given(/^I setup a deed with <(\d+)> borrowers$/) do |borrower|
   # Creates a deed, with entered number of [borrowers].
   step %(I have valid deed data with <#{borrower}> borrowers)
 end
--- features/testdebt_generic_test_data_step.feature
+++ features/testdebt_generic_test_data_step.feature
@@ -14,18 +14,9 @@ And borrower <1> views the deed
 # Readable way to override deed info
 @techdebt @techdebt2
 Scenario: WIP for override
-Given I setup deed with <1> borrowers
-Then set "forename" to "Timmy" for borrower <1>
+Given I setup a deed with <1> borrowers
+And I ammend "forename" to "Timmy" for borrower <1>
 And I create the deed
-And borrower <1> views the deed
-And the borrower <1> signature element is present on page
-Then the deed is digitally signed by borrower <1>
-
-# Sets up a 1 borower deed, with the single override applied
-# Used in a lot of existing tests
-@techdebt @techdebt3
-Scenario: WIP for override 2
-Given deed data with "Joe" as "forename"
 And borrower <1> views the deed
 And the borrower <1> signature element is present on page
 Then the deed is digitally signed by borrower <1>
