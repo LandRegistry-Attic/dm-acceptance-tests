b288f8466a666a65caf1fe497c092d7d7b12bb39
Aaron Flynn <aaronflynn@LRAdmins-MacBook-Pro.local>
2016-03-10 15:38:27 +0000

    Applied new Borrower Reference frontend

--- features/step_definitions/borrower_enters_reference.rb
+++ features/step_definitions/borrower_enters_reference.rb
@@ -1,10 +1,8 @@
 # Searches for deed with @borrower_token
 And(/^I search for the deed using the unique borrower reference$/) do
-
-  fill_in 'borrower_token', with: @borrower_token
-  #split_token = @borrower_token.split('-')
-  #fill_in 'borrower_token1', with: split_token[0]
-  #fill_in 'borrower_token2', with: split_token[1]
+  split_token = @borrower_token.split('-')
+  fill_in 'borrower_token', with: split_token[0]
+  fill_in 'borrower_token_2', with: split_token[1]
   click_button('Continue')
 end
 
