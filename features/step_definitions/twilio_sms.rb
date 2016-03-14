c42503d90c354a699cf493dafc933aac8027e708
Aaron Flynn <aaronflynn@LRAdmins-MacBook-Pro.local>
2016-03-14 15:33:45 +0000

    Tweaked the twilio steps

--- features/step_definitions/twilio_sms.rb
+++ features/step_definitions/twilio_sms.rb
@@ -2,27 +2,27 @@ require 'twilio-ruby'
 
 And(/^I get SMS$/) do
   # Get your Account Sid and Auth Token from twilio.com/user/account
-  account_sid = 'AC77e5da1ea0fea7ff8603ce4cc6e24002'
-  auth_token = '3c4c42eb9be58411f0d2e91edb853afb'
+  account_sid =
+  auth_token =
   @client = Twilio::REST::Client.new account_sid, auth_token
 
 # Get an object from its sid. If you do not have a sid,
 # check out the list resource examples on this page
-  @message = @client.account.messages.get("SM9d3941223eb842eebafd8c85f017ff0c")
+  @message = @client.account.messages.get("Insert_msg_ID")
   puts @message.body
 end
 
 And(/^I post SMS$/) do
   # put your own credentials here
-  account_sid = 'AC77e5da1ea0fea7ff8603ce4cc6e24002'
-  auth_token = '3c4c42eb9be58411f0d2e91edb853afb'
+  account_sid =
+  auth_token =
 
 # set up a client to talk to the Twilio REST API
   @client = Twilio::REST::Client.new account_sid, auth_token
 
   @client.account.messages.create({
                                       :from => '+447481362536',
-                                      :to => '07561137984',
+                                      :to => '',
                                       :body => 'Cucumber Test: Your HMLR code is: 123456',
                                   })
   puts 'SENT MESSAGE'
@@ -30,8 +30,8 @@ end
 
 And(/^I get message list$/) do
   # put your own credentials here
-  account_sid = 'AC77e5da1ea0fea7ff8603ce4cc6e24002'
-  auth_token = '3c4c42eb9be58411f0d2e91edb853afb'
+  account_sid =
+  auth_token =
 
 # set up a client to talk to the Twilio REST API
   @client = Twilio::REST::Client.new account_sid, auth_token
@@ -43,12 +43,14 @@ end
 
 And(/^I get latest message$/) do
   # put your own credentials here
-  account_sid = 'AC77e5da1ea0fea7ff8603ce4cc6e24002'
-  auth_token = '3c4c42eb9be58411f0d2e91edb853afb'
+  account_sid =
+  auth_token =
 
 # set up a client to talk to the Twilio REST API
   @client = Twilio::REST::Client.new account_sid, auth_token
 
-  @latest_msg = @client.account.messages.list[0].body
-  puts @latest_msg
+  @latest_msg = @client.account.messages.list[0].body.to_s
+  @auth_code = @latest_msg.split(' ')[-1]
+  puts "Auth code is: #{@auth_code}"
+
 end
