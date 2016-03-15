96ec078d962dd2772c05e054edda614921f7b6f0
Aaron Flynn <aaronflynn@LRAdmins-MacBook-Pro.local>
2016-03-15 09:58:47 +0000

    Minor changes, commenting and changes to feature file

--- features/step_definitions/twilio_sms.rb
+++ features/step_definitions/twilio_sms.rb
@@ -2,23 +2,23 @@ require 'twilio-ruby'
 
 And(/^I get SMS$/) do
   # Get your Account Sid and Auth Token from twilio.com/user/account
-  account_sid =
-  auth_token =
-  @client = Twilio::REST::Client.new account_sid, auth_token
+  @account_sid = ENV['TWILIO_ACCOUNT_SID']
+  @auth_token = ENV['TWILIO_AUTH_TOKEN']
+  @client = Twilio::REST::Client.new @account_sid, auth_token
 
 # Get an object from its sid. If you do not have a sid,
 # check out the list resource examples on this page
-  @message = @client.account.messages.get("Insert_msg_ID")
+  @message = @client.account.messages.get("[Insert_msg_ID]")
   puts @message.body
 end
 
 And(/^I post SMS$/) do
   # put your own credentials here
-  account_sid =
-  auth_token =
+  @account_sid = ENV['TWILIO_ACCOUNT_SID']
+  @auth_token = ENV['TWILIO_AUTH_TOKEN']
 
 # set up a client to talk to the Twilio REST API
-  @client = Twilio::REST::Client.new account_sid, auth_token
+  @client = Twilio::REST::Client.new @account_sid, @auth_token
 
   @client.account.messages.create({
                                       :from => '+447481362536',
@@ -30,11 +30,11 @@ end
 
 And(/^I get message list$/) do
   # put your own credentials here
-  account_sid =
-  auth_token =
+  @account_sid = ENV['TWILIO_ACCOUNT_SID']
+  @auth_token = ENV['TWILIO_AUTH_TOKEN']
 
 # set up a client to talk to the Twilio REST API
-  @client = Twilio::REST::Client.new account_sid, auth_token
+  @client = Twilio::REST::Client.new @account_sid, @auth_token
 
   @client.account.messages.list().each do |message|
     puts message.body
@@ -43,12 +43,11 @@ end
 
 And(/^I get latest message$/) do
   # put your own credentials here
-  account_sid =
-  auth_token =
-
+  account_sid = ENV['TWILIO_ACCOUNT_SID']
+  auth_token = ENV['TWILIO_AUTH_TOKEN']
 # set up a client to talk to the Twilio REST API
   @client = Twilio::REST::Client.new account_sid, auth_token
-
+# Place holder
   @latest_msg = @client.account.messages.list[0].body.to_s
   @auth_code = @latest_msg.split(' ')[-1]
   puts "Auth code is: #{@auth_code}"
--- features/twilio.feature
+++ features/twilio.feature
@@ -8,11 +8,6 @@ Feature: Checko Twilio API
 
   @wip1234
   Scenario: Set up deed with Twilio Phone Number
-  Given I setup a deed with <1> borrowers
-  And I amend "phone_number" to "07481362536" for borrower <1>
-  And I create the deed
+  Given I create default deed with <4> borrowers
   And borrower <1> views the deed
   Then the mortgage deed is displayed
-  Then the deed is digitally signed by borrower <1>
-  And borrower <1> views the deed
-  Then I verify borrower <1> has signed the deed
