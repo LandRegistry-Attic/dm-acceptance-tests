ab4f594a71e0c162e9d6ded27ad5643738b41c92
Dan Marker <daniel.marker@digital.landregistry.gov.uk>
2016-05-23 13:42:12 +0100

    Removal of text local api call

--- features/step_definitions/borrower_enters_authentication_code.rb
+++ features/step_definitions/borrower_enters_authentication_code.rb
@@ -1,14 +1,8 @@
 And(/^I get auth code for the borrower$/) do
-  api_key = ENV['TEXT_LOCAL_API_KEY']
-  inbox_id = ENV['TEXT_LOCAL_INBOX_ID']
 
-  request_url = 'http://api.txtlocal.com/get_messages/?' + 'apikey=' + api_key +
-                '&inbox_id=' + inbox_id
-  response = HTTP.get(request_url)
-  response_json = JSON.parse(response)
-  num_messages = response_json['messages'].length
-  split_msg = response_json['messages'][num_messages - 1]['message'].split(' ')
-  @auth_code = split_msg[0]
+  # Txtlocal has been removed from here
+
+  @auth_code = 'DM1234'
 end
 
 When(/^I enter the wrong authentication code$/) do
